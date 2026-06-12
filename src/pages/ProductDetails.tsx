04dfda50c026ea1ebcd918e76163003198b24a9a
Godwin <petergodwin437@icloud.com>
2026-06-12 13:13:58 +0300

    additional css in the page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -188,7 +188,7 @@ const ProductDetail: React.FC = () => {
         {/* Back Button */}
         <button 
           onClick={handleBackClick}
-          className="flex items-center text-blue-600 hover:text-blue-700 mb-6"
+          className="flex items-center text-blue-600 hover:text-blue-700 mb-6 cursor-pointer "
         >
           <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
